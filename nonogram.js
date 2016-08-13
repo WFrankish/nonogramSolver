252cfd6ff1d82405bfdabb2e87c9ba1f834a487e
WFrankish <wfrankish@btinternet.com>
2016-08-13 16:47:43 +0100

    Add HTML boiler plate and canvas

---
+++ nonogram.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+//ctx.fillStyle = "#FF0000";
+//ctx.fillRect(0, 0, 150, 75);
+
+console.log("JavaScript has loaded!");
